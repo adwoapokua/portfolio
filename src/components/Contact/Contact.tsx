f5bdd8458f25286cf95c66acb1fc8cd48d1311f5
adwoapokua <adwoapaboagye@gmail.com>
2026-05-12 02:05:45 +0000

    corrected contact page

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -3,11 +3,11 @@ import Mobile from "./mobile";
 
 export function Contact() {
   return (
-    <section id="contact" className="h-screen scroll-mt-20 flex flex-col ">
-        <div className="hidden lg:block mx-30 mt-30 mb-50">
+    <section id="contact" className="h-screen scroll-mt-20 flex flex-col md:mx-30 md:mt-30 md:mb-50">
+        <div className="hidden lg:block">
         <Desktop/>
       </div>
-      <div className="lg:hidden block mx-15 mt-10 mb-40">
+      <div className="lg:hidden block">
         <Mobile/>
       </div>
     </section>
